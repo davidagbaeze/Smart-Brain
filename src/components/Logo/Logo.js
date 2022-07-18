0f327e987ecae19d22741d43306213683832d373
David Agbaeze <you@example.com>
2022-07-18 02:26:08 +0100

    logo change

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Logo.css';
 import brain from "./brain.png";
-import Tilt from 'react-tilt';
+
 
 
 
@@ -9,9 +9,9 @@ import Tilt from 'react-tilt';
 const Logo = () => {
     return( 
         <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
-                <div className="Tilt-inner"><img style={{paddingTop: '15px'}} src={brain} alt='logo' /></div>
-            </Tilt>
+            <div className="color br2 shadow-3" style={{width: '100px'}}>
+            <img style={{padding: '15px'}} src={brain} alt='logo' />
+            </div>
         </div>
      );
 }
